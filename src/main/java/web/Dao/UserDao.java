e05cf3bc53c664b175d6208d3aab5461ae0bb343
YaroslavShcherbakov <dragna1945@gmail.com>
2023-07-22 16:09:02 +0300

    работает

---
+++ src/main/java/web/Dao/UserDao.java
@@ -0,0 +1,17 @@
+package web.Dao;
+
+import web.Model.User;
+
+import java.util.List;
+
+public interface UserDao {
+    void addUser(User user);
+
+    User getUser(int id);
+
+    void updateUser(int id, User user);
+
+    List<User> getAllUsers();
+
+    void deleteUser(int id);
+}
